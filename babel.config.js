31e866b344eddc6721f298469a5266cb488646dc
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-09-26 14:31:06 +0200

    [feat] Added absolut paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@config': './src/config',
+          '@core': './src/core',
+          '@hooks': './src/hooks',
+          '@infrasctructure': './src/infrastructure',
+          '@screens': './src/presentation/screens',
+          '@navigation': './src/presentation/navigation',
+          '@components': './src/presentation/components',
+        },
+      },
+    ],
+  ],
 };
