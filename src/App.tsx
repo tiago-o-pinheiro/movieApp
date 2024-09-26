f7428dd8a9c449c26a6396400ee566b7a0a3c59a
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-09-26 14:29:58 +0200

    [refactor] Moved application to src

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {StackNavigator} from '@navigation/StackNavigator';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
