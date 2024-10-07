e55b78ed83de4d2bc95b4b7292ce28766327446a
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-10-07 17:40:07 +0200

    [Feat] Added helper functions

--- src/infrastructure/mapper/movie.mapper.ts
+++ src/infrastructure/mapper/movie.mapper.ts
@@ -1,5 +1,9 @@
-import {Movie} from '@core/entities/movie.entity';
-import type {Result} from '@infrasctructure/interfaces/movie-db.responses';
+import {Cast, Movie, MovieDetail} from '@core/entities/movie.entity';
+import type {
+  CastResponse,
+  MovieDetailsResponse,
+  Result,
+} from '@infrasctructure/interfaces/movie-db.responses';
 
 export class MovieMapper {
   static fromMovieDBResponseToEntity(result: Result): Movie {
@@ -15,6 +19,31 @@ export class MovieMapper {
       backdrop: result.backdrop_path
         ? `https://image.tmdb.org/t/p/w500${result.backdrop_path}`
         : '',
+    };
+  }
+
+  static fromMovieDBResponseToEntityDetail(
+    movie: MovieDetailsResponse,
+  ): MovieDetail {
+    return {
+      id: movie.id,
+      title: movie.title,
+      description: movie.overview,
+      releaseDate: new Date(movie.release_date),
+      rating: movie.vote_average,
+      poster: movie.poster_path
+        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        : '',
+      backdrop: movie.backdrop_path
+        ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
+        : '',
+      genres: movie.genres,
+      duration: movie.runtime,
+      trailer: '',
+      budget: movie.budget,
+      originalTitle: movie.original_title,
+      voteAverage: movie.vote_average,
+      voteCount: movie.vote_count,
     };
   }
 }
