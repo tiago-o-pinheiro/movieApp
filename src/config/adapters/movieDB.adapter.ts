523e95b2ab7b12e8f1de56e7d21e818ba02aa7a9
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-10-07 17:33:27 +0200

    [feat] Added env to axios adapter

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,10 @@
+import {THE_MOVIE_DB_API_KEY} from '@env';
 import {AxiosAdapter} from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '924b930d66b3e028cca39a4514e1a18f',
+    api_key: THE_MOVIE_DB_API_KEY || 'no-api-key',
     language: 'es',
   },
 });
