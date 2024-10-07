25c4558113ff476b7c71216f00b47648f4489f1d
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-10-07 17:30:09 +0200

    [feat] Added env to project

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+    [
       'module-resolver',
       {
         alias: {
